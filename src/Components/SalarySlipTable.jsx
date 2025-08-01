dd7b7797aa706aa529dd6131edb84e0d18bc1619
Akshay Pratap Singh <akshay124-pixel@example.com>
2025-08-01 17:28:24 +0530

    Updated Fixs Final Download

--- src/Components/SalarySlipTable.jsx
+++ src/Components/SalarySlipTable.jsx
@@ -123,7 +123,7 @@ const SalarySlipTable = ({ salarySlips, onDelete }) => {
                   width: "10%",
                 }}
               >
-               Actions
+                Actions
               </th>
             </tr>
           </thead>
@@ -228,76 +228,84 @@ const SalarySlipTable = ({ salarySlips, onDelete }) => {
                           "0 3px 10px rgba(0, 0, 0, 0.15)";
                         e.target.style.transform = "scale(1)";
                       }}
+                      onClick={() =>
+                        console.log(`Downloading PDF from: ${slip.pdfUrl}`)
+                      } // Add logging
                       aria-label={`Download salary slip for ${slip.user} - ${slip.month}`}
-                      download={`${slip.user}-${slip.month}-salary-slip.pdf`}>
+                      download={`${slip.user}-${slip.month}-salary-slip.pdf`}
+                    >
                       Download
                     </a>
                   </td>
-                  <td style={{ padding: "1.4rem 2rem", justifyContent:"center",alignItems:"center",display:"flex" }}>
-                  <button
-                        className="bin-button"
-                        onClick={() => onDelete(slip._id)}
-                        style={{
-                          width: "40px",
-                          height: "40px",
-                          borderRadius: "50%",
-                          padding: "0",
-                          display: "flex",
-                          alignItems: "center",
-                          justifyContent: "center",
-                        }}
+                  <td
+                    style={{
+                      padding: "1.4rem 2rem",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      display: "flex",
+                    }}
+                  >
+                    <button
+                      className="bin-button"
+                      onClick={() => onDelete(slip._id)}
+                      style={{
+                        width: "40px",
+                        height: "40px",
+                        borderRadius: "50%",
+                        padding: "0",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <svg
+                        className="bin-top"
+                        viewBox="0 0 39 7"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
                       >
-                        <svg
-                          className="bin-top"
-                          viewBox="0 0 39 7"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <line
-                            y1="5"
-                            x2="39"
-                            y2="5"
-                            stroke="white"
-                            strokeWidth="4"
-                          ></line>
-                          <line
-                            x1="12"
-                            y1="1.5"
-                            x2="26.0357"
-                            y2="1.5"
-                            stroke="white"
-                            strokeWidth="3"
-                          ></line>
-                        </svg>
-                        <svg
-                          className="bin-bottom"
-                          viewBox="0 0 33 39"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <mask id="path-1-inside-1_8_19" fill="white">
-                            <path d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"></path>
-                          </mask>
-                          <path
-                            d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"
-                            fill="white"
-                            mask="url(#path-1-inside-1_8_19)"
-                          ></path>
-                          <path
-                            d="M12 6L12 29"
-                            stroke="white"
-                            strokeWidth="4"
-                          ></path>
-                          <path
-                            d="M21 6V29"
-                            stroke="white"
-                            strokeWidth="4"
-                          ></path>
-                        </svg>
-                      </button>
-
-
-
+                        <line
+                          y1="5"
+                          x2="39"
+                          y2="5"
+                          stroke="white"
+                          strokeWidth="4"
+                        ></line>
+                        <line
+                          x1="12"
+                          y1="1.5"
+                          x2="26.0357"
+                          y2="1.5"
+                          stroke="white"
+                          strokeWidth="3"
+                        ></line>
+                      </svg>
+                      <svg
+                        className="bin-bottom"
+                        viewBox="0 0 33 39"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <mask id="path-1-inside-1_8_19" fill="white">
+                          <path d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"></path>
+                        </mask>
+                        <path
+                          d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"
+                          fill="white"
+                          mask="url(#path-1-inside-1_8_19)"
+                        ></path>
+                        <path
+                          d="M12 6L12 29"
+                          stroke="white"
+                          strokeWidth="4"
+                        ></path>
+                        <path
+                          d="M21 6V29"
+                          stroke="white"
+                          strokeWidth="4"
+                        ></path>
+                      </svg>
+                    </button>
                   </td>
                 </tr>
               ))
